05c7a6bba4807dd05ebd4543ef7fba19fb7ff2bc
jungjuyoung <nwwn1004@naver.com>
2023-11-14 03:00:32 +0900

    installed timeago

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,45 @@
-import { FullPost } from "@/model/post";
+import { SimplePost } from "@/model/post";
+import Avatar from "./Avatar";
+import Image from "next/image";
+import { HeartIcon } from "./ui/icons";
+import BookmarkIcon from "./ui/icons/BookmarkIcon";
+import parseDate from "@/util/date";
+import SmileIcon from "./ui/icons/SmileIcon";
 
 type Props = {
-  data: FullPost
+  post: SimplePost;
 };
-export default function PostCard({ data }: Props) {
-  return <div>PostCard</div>;
+export default function PostCard({ post }: Props) {
+  const { userImage, username, image, createdAt, likes, text } = post;
+  return (
+    <>
+      <div>
+        <Avatar username={username} image={image} highlight />
+        <span>{username}</span>
+      </div>
+      <Image
+        src={image}
+        width={500}
+        height={500}
+        alt={`photo by ${username}`}
+      />
+      <div>
+        <HeartIcon />
+        <BookmarkIcon />
+      </div>
+      <div>
+        <p>{`${likes?.length ?? 0} ${likes?.length > 1 ? "likes" : "like"}`}</p>
+        <p>
+          <span>{username}</span>
+          {text}
+        </p>
+        <p>{parseDate(createdAt)}</p>
+        <form>
+          <SmileIcon />
+          <input type="text" placeholder="Add a comment" />
+          <button>Post</button>
+        </form>
+      </div>
+    </>
+  );
 }
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,17 +1,28 @@
 "use client";
 import PostCard from "@/components/PostCard";
 import { SimplePost } from "@/model/post";
+import { GridLoader } from "react-spinners";
 import useSWR from "swr";
 
 export default function PostList() {
   const { isLoading, data: posts, error } = useSWR<SimplePost[]>("/api/post");
-  console.log(posts);
+  // console.log(posts);
   return (
-    <div>
-      {/* <PostCard data={data} /> */}
-      <ul>
-        {posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}
-      </ul>
-    </div>
+    <section>
+      {isLoading && (
+        <div>
+          <GridLoader color="red" />
+        </div>
+      )}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <PostCard post={post} />{" "}
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
   );
 }
