e478505570747b86a5a58865fffe7123aa9c1756
jungjuyoung <nwwn1004@naver.com>
2023-12-08 02:06:47 +0900

    feat: FollowButton implement

--- src/components/FollowerButton.tsx
+++ src/components/FollowerButton.tsx
@@ -3,6 +3,9 @@
 import { ProfileUser } from "@/model/user";
 import useMe from "@/hook/useMe";
 import Button from "./ui/Button";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { PulseLoader } from "react-spinners";
 
 type Props = {
   user: ProfileUser;
@@ -11,24 +14,43 @@ export default function FollowerButton({ user }: Props) {
   const { username } = user;
   const { user: loggedInUser, toggleFollow } = useMe();
 
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isUpdating = isPending || isFetching;
+
   const showButton = loggedInUser && loggedInUser.username !== username;
   const following =
     loggedInUser &&
     loggedInUser.following.find((item) => item.username === username);
-  
+  // console.log(loggedInUser);
+
   const text = following ? "Unfollow" : " Follow";
 
-  const handleFollow = () => {
-    toggleFollow(user.id, !following);
+  const handleFollow = async () => {
+    setIsFetching(true);
+    await toggleFollow(user.id, !following);
+    setIsFetching(false);
+    startTransition(() => {
+      router.refresh();
+    });
   };
   return (
     <>
       {showButton && (
-        <Button
-          text={text}
-          onClick={handleFollow}
-          red={text === "Unfollow"}
-        />
+        <div className="relative">
+          {isUpdating && (
+            <div className="absolute flex inset-0 z-10 justify-center items-center">
+              <PulseLoader size={6} />
+            </div>
+          )}
+          <Button
+            disabled={isUpdating}
+            text={text}
+            onClick={handleFollow}
+            red={text === "Unfollow"}
+          />
+        </div>
       )}
     </>
   );
